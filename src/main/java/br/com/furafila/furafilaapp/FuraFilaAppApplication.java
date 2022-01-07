5f4238d767d47083f5a695c96ae74e302383ffee
Wellington PIres <wellington.goncalves@live.com>
2022-01-07 10:18:59 -0300

    First commit

---
+++ src/main/java/br/com/furafila/furafilaapp/FuraFilaAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.furafila.furafilaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuraFilaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuraFilaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/furafila/furafilaapp/FuraFilaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.furafila.furafilaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuraFilaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
